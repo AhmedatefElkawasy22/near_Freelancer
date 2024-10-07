d004d59603afc0e30678326d3c417b91233e1388
AhmedatefElkawasy22 <elkawasyahmed@gamil.com>
2024-10-07 15:29:25 +0300

    main branch project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
